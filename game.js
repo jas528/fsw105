2fb46c1140a8dd5d84500ad483d67301b9af114f
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-04-07 12:19:12 -0500

    message

--- game.js
+++ game.js
@@ -1,20 +1,20 @@
 const readlineSync = require ('readline-sync');
 
-const name = readlineSync.question( 'What is your name?');
+const john = readlineSync.question( 'What is your name?');
 
-readlineSync.question ('Hello'+ name + ', Welcome to Doom!  You will risk your life to defeat Bees. Press Enter');
+readlineSync.question ('Hello'+ john + ', Welcome to Doom!  You will risk your life to defeat Bees. Press Enter');
 
-const badGuys  = ['Killer Bee', 'Monster', 'Queen Bee', 'Sting Monster'];
-const treasure = ['Medkit','Security Armor', 'Bee Pack', 'Health Postion'];
+const   = ['Killer Bees', 'Stinger', 'Queen Bee', 'Sting Monster'];
+const treasure = ['Comb','Honey', 'Bee Pack', 'Health Postion'];
 var prize = [];
 let userHealth = 30;
-const option = ["Run",'Exit','Print' ];
+const option = ["Run",'Exit','Hide' ];
 let pickUp = treasure[ Math.floor(Math.random() *(5-1+ 2)* 3)]
 function game(){
 const attactPower = Math.floor(Math.random() *(5-4+3)*5);
-const badGuy = badGuys [Math.floor(Math.random() *badGuys.length)];
+const killerBees = killerBees [Math.floor(Math.random() *killerBees.length)];
 let badGuysHealth= 58;
-const badGuyPower= Math.floor(Math.random() *(5-3+2)*5)
+const killerBeesPower= Math.floor(Math.random() *(5-3+2)*5)
 
 const index =readlineSync.keyInSelect(options,'What would you like to do next?');
 
@@ -26,16 +26,20 @@ console.log (name + ' : n tresure: '+ pickUp);
     let key =Math.random ();
     if (key <= .3) {
         console.log('Walking...');
-    }  else if (key >= .3 ){
-    console.log(badGuy +' showed up.');
+    } 
+     else if (killerBees +' showed up.');
 
-    while(badguyshealth > 0 && userHealth > 0) {
+   while(killerBeeshealth > 0 && userHealth > 0) {
 
-        const user = readlineSync.question()
+    const user = readlineSync.question()
     
 switch(user){
     case 'r'://runaway
     const run = Math.random();
     if(run <.5) {
-        console.log 
-    }}}}}
+        console.log ('Before you can run and hide' + killerBees + 'sting you with:' + killerBeesPower);
+    }else {
+        console.log ('Before you can run and hide'+ killerBees + 'attacks you with: '+killerBeesPower);
+    }}else {console
+        console.log('Run Away!!!');
+        break;}}
